ab18ed170d16bbd230fcabc34f26bf12bac3d0fd
Paul Lee <contact@paullee.dev>
2021-02-21 18:10:49 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, redirect, send_file, render_template, request
+import requests
+import random
+import io
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=data)
+
+# rock -> 0
+# scissors -> 1
+# paper -> 2
+
+def winner(a, b):
+    if a == b:
+        return None
+    if a == 0 and b == 1:
+        return True
+    if a == 1 and b == 2:
+        return True
+    if a == 2 and b == 0:
+        return True
+    return False
+
+@app.route('/game')
+def game():
+    move = int(request.args.get('move'))
+    server_move = random.randint(0, 3)
+    result = winner(move, server_move)
+    return render_template('game.html', move=move, server_move=server_move, result=result)
+
+app.run('0.0.0.0', port=8000)
